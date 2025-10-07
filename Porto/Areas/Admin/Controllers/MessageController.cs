6b0cde1ede0c6d0af8e5e587706fdbcbe11cdbbb
JuliaSimonyan <julietasimonyan2001@mail.ru>
2025-10-07 18:06:50 +0400

    Admin panel adjustments

---
+++ Porto/Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Porto.Data.Models;
+
+namespace Porto.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class MessageController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public MessageController(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var messages = await _context.ChatMessages
+               .Include(m => m.User)
+               .OrderBy(m => m.Timestamp)
+               .ToListAsync();
+
+            return View(messages);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var message = await _context.ChatMessages.FindAsync(id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+            _context.ChatMessages.Remove(message);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+    }
+}
